97f70796ce99e30523f5774b8e7150a7161e0a11
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2024-03-21 17:53:21 +0100

    build: install and configure tailwindcss and daisyUI

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div class="text-center my-10 prose">
+      <h1>Hello world !</h1>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
